49c5773cab3009df74d0364a5f262485846d1a38
jojow <johannes.weizenegger@hm.edu>
2024-06-22 20:03:02 +0200

    pages updated

--- lib/pages/club_page.dart
+++ lib/pages/club_page.dart
@@ -14,9 +14,16 @@ class ClubPage extends StatefulWidget {
 
 class _ClubPageState extends State<ClubPage> {
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _streetController = TextEditingController();
-  final TextEditingController _houseNumberController = TextEditingController();
+  final TextEditingController _clubNameController = TextEditingController();
+  final TextEditingController _clubStreetController = TextEditingController();
+  final TextEditingController _clubHouseNumberController = TextEditingController();
+  final TextEditingController _clubZipCodeController = TextEditingController();
+  final TextEditingController _clubCityController = TextEditingController();
+  final TextEditingController _ownerNameController = TextEditingController();
+  final TextEditingController _ownerStreetController = TextEditingController();
+  final TextEditingController _ownerHouseNumberController = TextEditingController();
+  final TextEditingController _ownerZipCodeController = TextEditingController();
+  final TextEditingController _ownerCityController = TextEditingController();
   File? _selectedImage;
 
   @override
@@ -31,8 +38,105 @@ class _ClubPageState extends State<ClubPage> {
           key: _formKey,
           child: ListView(
             children: [
+              const Text(
+                'Angaben zum Club',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
               TextFormField(
-                controller: _nameController,
+                controller: _clubNameController,
+                decoration: const InputDecoration(
+                  labelText: "Name des Clubs",
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Bitte geben Sie den Namen des Clubs ein';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Adresse:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _clubStreetController,
+                      decoration: const InputDecoration(
+                        labelText: "Straße",
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Bitte geben Sie Ihre Straße ein';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  SizedBox(
+                    width: 80,
+                    child: TextFormField(
+                      controller: _clubHouseNumberController,
+                      decoration: const InputDecoration(
+                        labelText: "Hausnr.",
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Bitte geben Sie Ihre Hausnummer ein';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _clubZipCodeController,
+                      decoration: const InputDecoration(
+                        labelText: "PLZ",
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Bitte geben Sie Ihre Postleitzahl ein';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: TextFormField(
+                      controller: _clubCityController,
+                      decoration: const InputDecoration(
+                        labelText: "Ort",
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Bitte geben Sie Ihren Ort ein';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Angaben zum Clubinhaber',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _ownerNameController,
                 decoration: const InputDecoration(
                   labelText: "Name",
                 ),
@@ -44,34 +148,77 @@ class _ClubPageState extends State<ClubPage> {
                 },
               ),
               const SizedBox(height: 16),
-              TextFormField(
-                controller: _streetController,
-                decoration: const InputDecoration(
-                  labelText: "Straße",
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Bitte geben Sie Ihre Straße ein';
-                  }
-                  return null;
-                },
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _ownerStreetController,
+                      decoration: const InputDecoration(
+                        labelText: "Straße",
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Bitte geben Sie Ihre Straße ein';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  SizedBox(
+                    width: 80,
+                    child: TextFormField(
+                      controller: _ownerHouseNumberController,
+                      decoration: const InputDecoration(
+                        labelText: "Hausnr.",
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Bitte geben Sie Ihre Hausnummer ein';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(height: 16),
-              TextFormField(
-                controller: _houseNumberController,
-                decoration: const InputDecoration(
-                  labelText: "Hausnummer",
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Bitte geben Sie Ihre Hausnummer ein';
-                  }
-                  return null;
-                },
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _ownerZipCodeController,
+                      decoration: const InputDecoration(
+                        labelText: "PLZ",
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Bitte geben Sie Ihre Postleitzahl ein';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: TextFormField(
+                      controller: _ownerCityController,
+                      decoration: const InputDecoration(
+                        labelText: "Ort",
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Bitte geben Sie Ihren Ort ein';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(height: 16),
               const Text(
-                'Zur Verifizierung benötigen wir noch ein Bild Ihres Ausweises.',
+                'Zur vollständigen Registrierung benötigen wir ein Bild Ihres Personalausweises.',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 16),
@@ -132,9 +279,16 @@ class _ClubPageState extends State<ClubPage> {
     }
 
     final String userId = user.uid;
-    final String name = _nameController.text;
-    final String street = _streetController.text;
-    final String houseNumber = _houseNumberController.text;
+    final String clubName = _clubNameController.text;
+    final String clubStreet = _clubStreetController.text;
+    final String clubHouseNumber = _clubHouseNumberController.text;
+    final String clubZipCode = _clubZipCodeController.text;
+    final String clubCity = _clubCityController.text;
+    final String ownerName = _ownerNameController.text;
+    final String ownerStreet = _ownerStreetController.text;
+    final String ownerHouseNumber = _ownerHouseNumberController.text;
+    final String ownerZipCode = _ownerZipCodeController.text;
+    final String ownerCity = _ownerCityController.text;
     final String uniqueFileName = DateTime.now().millisecondsSinceEpoch.toString();
     final Reference referenceRoot = FirebaseStorage.instance.ref();
     final Reference referenceDirImages = referenceRoot.child('images');
@@ -144,11 +298,40 @@ class _ClubPageState extends State<ClubPage> {
       await referenceImageToUpload.putFile(_selectedImage!);
       final String imageUrl = await referenceImageToUpload.getDownloadURL();
 
-      await FirebaseFirestore.instance.collection('club').doc(userId).set({
-        'name': name,
-        'street': street,
-        'house_number': houseNumber,
+      // Speichere die Daten in der Sammlung newClubRequests
+      await FirebaseFirestore.instance.collection('newClubRequest').doc(userId).set({
+        'club_name': clubName,
+        'club_street': clubStreet,
+        'club_house_number': clubHouseNumber,
+        'club_zip_code': clubZipCode,
+        'club_city': clubCity,
+        'owner_name': ownerName,
+        'owner_street': ownerStreet,
+        'owner_house_number': ownerHouseNumber,
+        'owner_zip_code': ownerZipCode,
+        'owner_city': ownerCity,
         'image_url': imageUrl,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+
+      // Erstelle einen neuen Eintrag in der Sammlung owners und erhalte die Dokument-ID
+      DocumentReference ownerRef = await FirebaseFirestore.instance.collection('owner').add({
+        'name': ownerName,
+        'street': ownerStreet,
+        'house_number': ownerHouseNumber,
+        'zip_code': ownerZipCode,
+        'city': ownerCity,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+
+      // Speichere die Daten in der Sammlung clubs mit einer Referenz zum Owner
+      await FirebaseFirestore.instance.collection('club').add({
+        'name': clubName,
+        'street': clubStreet,
+        'house_number': clubHouseNumber,
+        'zip_code': clubZipCode,
+        'city': clubCity,
+        'owner_id': ownerRef.id, // Referenz auf den Owner
         'timestamp': FieldValue.serverTimestamp(),
       });
 
@@ -156,9 +339,16 @@ class _ClubPageState extends State<ClubPage> {
         const SnackBar(content: Text('Daten erfolgreich gespeichert')),
       );
 
-      _nameController.clear();
-      _streetController.clear();
-      _houseNumberController.clear();
+      _clubNameController.clear();
+      _clubStreetController.clear();
+      _clubHouseNumberController.clear();
+      _clubZipCodeController.clear();
+      _clubCityController.clear();
+      _ownerNameController.clear();
+      _ownerStreetController.clear();
+      _ownerHouseNumberController.clear();
+      _ownerZipCodeController.clear();
+      _ownerCityController.clear();
       setState(() {
         _selectedImage = null;
       });
@@ -171,9 +361,16 @@ class _ClubPageState extends State<ClubPage> {
 
   @override
   void dispose() {
-    _nameController.dispose();
-    _streetController.dispose();
-    _houseNumberController.dispose();
+    _clubNameController.dispose();
+    _clubStreetController.dispose();
+    _clubHouseNumberController.dispose();
+    _clubZipCodeController.dispose();
+    _clubCityController.dispose();
+    _ownerNameController.dispose();
+    _ownerStreetController.dispose();
+    _ownerHouseNumberController.dispose();
+    _ownerZipCodeController.dispose();
+    _ownerCityController.dispose();
     super.dispose();
   }
 }
