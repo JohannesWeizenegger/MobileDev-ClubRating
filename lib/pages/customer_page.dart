17de7986a5dd8251f91f7897a3aca88f16270a4f
stadl <stadler.moritz@hm.edu>
2024-06-23 18:13:45 +0200

    Without Loader

--- lib/pages/customer_page.dart
+++ lib/pages/customer_page.dart
@@ -27,8 +27,15 @@ class CustomerPage extends StatefulWidget {
 class _CustomerPageState extends State<CustomerPage> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  // Beispieluser-Id, in einer realen Anwendung würde diese aus der Authentifizierung kommen
   final String userId = "exampleUserId";
+  late List<Map<String, dynamic>> clubs = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAllClubData();
+  }
 
   Future<void> saveOrUpdateRating(String clubId, int rating) async {
     QuerySnapshot snapshot = await _firestore
@@ -40,7 +47,6 @@ class _CustomerPageState extends State<CustomerPage> {
         .get();
 
     if (snapshot.docs.isEmpty) {
-      // Wenn keine Bewertung vorhanden ist, füge eine neue Bewertung hinzu
       await _firestore
           .collection('club')
           .doc(clubId)
@@ -51,13 +57,17 @@ class _CustomerPageState extends State<CustomerPage> {
         'timestamp': FieldValue.serverTimestamp(),
       });
     } else {
-      // Wenn eine Bewertung vorhanden ist, aktualisiere sie
       DocumentReference ratingDoc = snapshot.docs.first.reference;
       await ratingDoc.update({
         'rating': rating,
         'timestamp': FieldValue.serverTimestamp(),
       });
     }
+
+    // Aktualisieren die Liste Club-Daten hier
+    setState(() {});
+    await fetchAllClubData();
+    setState(() {});
   }
 
   Future<double> getAverageRating(String clubId) async {
@@ -100,14 +110,14 @@ class _CustomerPageState extends State<CustomerPage> {
     return snapshot.docs.first['rating'] as int?;
   }
 
-  Future<List<Map<String, dynamic>>> fetchAllClubData() async {
+  Future<void> fetchAllClubData() async {
     QuerySnapshot clubSnapshot = await _firestore.collection('club').get();
-    List<Map<String, dynamic>> clubs = [];
+    List<Map<String, dynamic>> fetchedClubs = [];
     for (var club in clubSnapshot.docs) {
       var avgRating = await getAverageRating(club.id);
       var ratingCount = await getRatingCount(club.id);
       var userRating = await getUserRating(club.id);
-      clubs.add({
+      fetchedClubs.add({
         'data': club.data(),
         'id': club.id,
         'avgRating': avgRating,
@@ -115,7 +125,10 @@ class _CustomerPageState extends State<CustomerPage> {
         'userRating': userRating,
       });
     }
-    return clubs;
+    setState(() {
+      clubs = fetchedClubs;
+      isLoading = false;
+    });
   }
 
   @override
@@ -124,106 +137,95 @@ class _CustomerPageState extends State<CustomerPage> {
       appBar: AppBar(
         title: const Text("Verkäufer"),
       ),
-      body: FutureBuilder<List<Map<String, dynamic>>>(
-        future: fetchAllClubData(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : clubs.isEmpty
+              ? const Center(child: Text("Keine Clubs gefunden."))
+              : ListView.builder(
+                  itemCount: clubs.length,
+                  itemBuilder: (context, index) {
+                    final club = clubs[index];
+                    final userRating = club['userRating'];
+                    final bool hasRated = userRating != null;
 
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text("Keine Clubs gefunden."));
-          }
+                    return ListTile(
+                      title: Text(club['data']['name'] ?? 'N/A'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                              '${club['data']['street'] ?? 'N/A'} ${club['data']['house_number'] ?? ''}'),
+                          Text(
+                              '${club['data']['zip_code'] ?? 'N/A'} ${club['data']['city'] ?? ''}'),
+                          Row(
+                            children: [
+                              Text('${club['avgRating'].toStringAsFixed(1)}'),
+                              SizedBox(width: 8),
+                              Row(
+                                children: List.generate(5, (index) {
+                                  double starRating = index + 1;
+                                  double fillPercentage = 0.0;
 
-          final clubs = snapshot.data!;
+                                  double remainder =
+                                      club['avgRating'] - (starRating - 1);
+                                  if (remainder >= 0.2 && remainder <= 0.4) {
+                                    fillPercentage = 0.4;
+                                  } else if (remainder == 0.5) {
+                                    fillPercentage = 0.5;
+                                  } else if (remainder >= 0.6 &&
+                                      remainder <= 0.8) {
+                                    fillPercentage = 0.6;
+                                  } else if (remainder >= 0.9) {
+                                    fillPercentage = 1.0;
+                                  } else if (remainder > 0) {
+                                    fillPercentage = remainder;
+                                  }
 
-          return ListView.builder(
-            itemCount: clubs.length,
-            itemBuilder: (context, index) {
-              final club = clubs[index];
-              final userRating = club['userRating'];
-
-              return ListTile(
-                title: Text(club['data']['name'] ?? 'N/A'),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                        '${club['data']['street'] ?? 'N/A'} ${club['data']['house_number'] ?? ''}'),
-                    Text(
-                        '${club['data']['zip_code'] ?? 'N/A'} ${club['data']['city'] ?? ''}'),
-                    Row(
-                      children: [
-                        Text('${club['avgRating'].toStringAsFixed(1)}'),
-                        SizedBox(width: 8),
-                        Row(
-                          children: List.generate(5, (index) {
-                            double starRating = index + 1;
-                            double fillPercentage = 0.0;
-
-                            double remainder =
-                                club['avgRating'] - (starRating - 1);
-                            if (remainder >= 0.2 && remainder <= 0.4) {
-                              fillPercentage = 0.4;
-                            } else if (remainder == 0.5) {
-                              fillPercentage = 0.5;
-                            } else if (remainder >= 0.6 && remainder <= 0.8) {
-                              fillPercentage = 0.6;
-                            } else if (remainder >= 0.9) {
-                              fillPercentage = 1.0;
-                            } else if (remainder > 0) {
-                              fillPercentage = remainder;
-                            }
-
-                            return Stack(
-                              children: [
-                                Icon(
-                                  Icons.star_border,
-                                  color: Colors.amber,
-                                ),
-                                ClipRect(
-                                  clipper: StarClipper(fillPercentage),
-                                  child: Icon(
-                                    Icons.star,
+                                  return Stack(
+                                    children: [
+                                      Icon(
+                                        Icons.star_border,
+                                        color: Colors.amber,
+                                      ),
+                                      ClipRect(
+                                        clipper: StarClipper(fillPercentage),
+                                        child: Icon(
+                                          Icons.star,
+                                          color: Colors.amber,
+                                        ),
+                                      ),
+                                    ],
+                                  );
+                                }),
+                              ),
+                              Text(' (${club['ratingCount']} Bewertungen)'),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const Text('Bewertung: '),
+                              for (int i = 1; i <= 5; i++)
+                                IconButton(
+                                  icon: Icon(
+                                    i <= (userRating ?? 0)
+                                        ? Icons.star
+                                        : Icons.star_border,
                                     color: Colors.amber,
                                   ),
-                                ),
-                              ],
-                            );
-                          }),
-                        ),
-                        Text(' (${club['ratingCount']} Bewertungen)'),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        const Text('Bewertung: '),
-                        for (int i = 1; i <= 5; i++)
-                          IconButton(
-                            icon: Icon(
-                              i <= (userRating ?? 0)
-                                  ? Icons.star
-                                  : Icons.star_border,
-                              color: Colors.amber,
-                            ),
-                            onPressed: () {
-                              saveOrUpdateRating(club['id'], i);
-                              setState(
-                                  () {}); // Aktualisiere das UI nach dem Speichern der Bewertung
-                            },
-                          )
-                      ],
-                    ),
-                  ],
+                                  onPressed: () {
+                                    saveOrUpdateRating(club['id'], i);
+                                  },
+                                )
+                            ],
+                          ),
+                        ],
+                      ),
+                      onTap: () {
+                        // Hier könntest du eine Detailseite aufrufen oder weitere Aktionen durchführen
+                      },
+                    );
+                  },
                 ),
-                onTap: () {
-                  // Hier könntest du eine Detailseite aufrufen oder weitere Aktionen durchführen
-                },
-              );
-            },
-          );
-        },
-      ),
     );
   }
 }
